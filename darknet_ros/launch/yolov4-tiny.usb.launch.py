39ca50ef039090bb793fee91e8295cd4ad4b84bf
zeta0707 <zeta0707@gmail.com>
2023-07-08 19:18:41 +0900

    Implement yolov4-tiny.x.launch.py
    1. yolov4-tiny.csi.launch.py calls darknet_ros.csi.launch.py
    2. yolov4-tiny.usb.launch.py calls darknet_ros.usb.launch.py
    3. Delete unnecesssary yaml

---
+++ darknet_ros/launch/yolov4-tiny.usb.launch.py
@@ -0,0 +1,25 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+def generate_launch_description():
+  darknet_ros_share_dir = get_package_share_directory('darknet_ros')
+  network_param_file = darknet_ros_share_dir + '/config/yolov4-tiny.yaml'
+
+  darknet_ros_launch = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource([darknet_ros_share_dir + '/launch/darknet_ros.usb.launch.py']),
+      launch_arguments={'network_param_file': network_param_file}.items()
+  )
+
+  camera = Node(
+    package="monicar_cv",
+    executable="webcam_pub",
+    )
+ 
+  return LaunchDescription([
+    darknet_ros_launch,
+    # if you want to disable camera node, remove the following line.
+    camera,
+  ])
