31269406ac5fe9bd0165bfe6acbbbae52e0bd564
zeta0707 <zeta0707@gmail.com>
2023-07-11 18:38:29 +0900

    Remove camera topic remap
    1. Delete remapping of camera topic. camera package will sync to use image_raw
    2. Thus csi, usb launch.py are not required
    3.Don't run camera node from yolov4-tiny, camera package needs to run

--- darknet_ros/launch/darknet_ros.launch.py
+++ darknet_ros/launch/darknet_ros.launch.py
@@ -43,9 +43,6 @@ def generate_launch_description():
     executable='darknet_ros',
     name='darknet_ros',
     output='screen',
-    remappings=[
-        ('/image_raw', 'csi_image'),
-    ],
     
     parameters=[ros_param_file, network_param_file,
       {
